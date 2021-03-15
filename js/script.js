22f23a55f0d8d59366da4329f2867f4b1f8ff0b7
glensorbo <pookiecode@gmail.com>
2021-03-15 13:22:20 +0100

    Adds question 2 + missing HTML

--- js/script.js
+++ js/script.js
@@ -30,6 +30,9 @@ const cat = {
  * Change its innerHTML value to "Updated heading".
  */
 
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
 /*
  * Question 3:
  * Use the style property on the heading variable from the question above to change its font size to "2em".
